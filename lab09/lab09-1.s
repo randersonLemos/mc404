e5c0ae585a18124f895a725518bfb5921f59e355
Randerson Lemos <randerson.lemos@gmail.com>
2023-05-17 20:43:18 -0300

    Started lab09

--- lab09/lab09-1.s
+++ lab09/lab09-1.s
@@ -10,16 +10,54 @@ main:
   ret
 
 LeNumero:
-  addi sp, sp, -4
-  sw ra, 0(sp)
+  addi sp, sp, -40 # Reservando 32 bytes pra string
+  sw s0, 36(sp)
+  sw ra, 32(sp)
+  mv s0, sp # Comeco da string de 32 bytes
 
-  la s1, vec1
+  mv a0, s0
+  call LeString
 
-  li a0, 1
-  lw 
-  
-  ecall
+  mv t0, s0
+LoopLeNumero:
+  lbu t1, 0(t0)
+  beq t1, zero, FimLeNumero
+    li a0, 11
+    mv a1, t1
+    ecall
+    addi t0, t0, 1 
+    j LoopLeNumero 
 
-  lw ra, 0(sp)
-  addi sp, sp, 4
+    
+FimLeNumero:
+  lw ra, 32(sp)
+  lw s0, 36(sp)
+  addi sp, sp, 440
   ret
+
+LeString:
+    addi  sp, sp, -8
+    sw    s0, 4(sp)
+    sw    ra, 0(sp)
+
+    mv    s0, a0
+    li    a0, 0x130
+    ecall        
+
+LoopLeString: 
+    li    a0, 0x131 
+    ecall
+
+    beq   a0, zero, FimLeString # se for zero, terminou de ler
+    li    t0, 1
+    beq   a0, t0, LoopLeString
+    sb    a1, 0(s0)
+    addi  s0, s0, 1
+    j     LoopLeString
+
+FimLeString: 
+    sb    zero, 0(s0) # coloca o zero no final da string
+    lw    ra, 0(sp)
+    lw    s0, 4(sp)
+    addi  sp, sp, 8
+    ret
